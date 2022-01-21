383e770f9f866fa6c5284de18f2eaba6bc10baca
Sangyun <tkfkd4447@naver.com>
2022-01-21 16:51:27 +0900

    1697

--- 단계별로풀어보기/DFS_BFS/숨바꼭질/1697.py
+++ 단계별로풀어보기/DFS_BFS/숨바꼭질/1697.py
@@ -1,26 +1,25 @@
 from collections import deque
 
-MAX = 100000
-
 n,k = map(int,input().split())
-
-graph = [0]*(MAX+1)
 
 queue = deque()
 queue.append(n)
+
+MAX = 100000
+
+graph = [0]*(MAX+1)
 
 def bfs():
     while queue:
         x = queue.popleft()
         if x == k:
             break
-        
         for i in (x+1,x-1,2*x):
             if 0<=i<=MAX and graph[i]==0:
-                queue.append(i)
                 graph[i] = graph[x]+1
-
+                queue.append(i)
 
 bfs()
 
 print(graph[k])
+
